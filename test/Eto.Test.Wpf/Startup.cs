dde62820df88da1646ff369531b1f3c1f40caeca
philstopford <phil.stopford@gmail.com>
2021-04-16 09:06:45 -0500

    Following Curtis' advice to avoid setting background automatically.

--- test/Eto.Test.Wpf/Startup.cs
+++ test/Eto.Test.Wpf/Startup.cs
@@ -20,6 +20,8 @@ namespace Eto.Test.Wpf
 			var app = new TestApplication(platform);
 			app.TestAssemblies.Add(typeof(Startup).Assembly);
 
+			Style.Add<Eto.Forms.Window>(null, w => w.BackgroundColor = Eto.Drawing.Colors.Transparent);
+
 			System.Windows.Application.Current.Resources.MergedDictionaries.Add(new System.Windows.ResourceDictionary { Source = new Uri("pack://application:,,,/DynamicAero2;component/Theme.xaml", UriKind.RelativeOrAbsolute) });
 			System.Windows.Application.Current.Resources.MergedDictionaries.Add(new System.Windows.ResourceDictionary { Source = new Uri("pack://application:,,,/DynamicAero2;component/Brushes/Dark.xaml", UriKind.RelativeOrAbsolute) });
 
